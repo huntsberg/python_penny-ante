8b1980e91059544fde3189a9986ab70f404333e3
Bowen, Peter <pbowen@conversantmedia.com>
2024-03-11 22:34:22 -0700

    Added Player Class and tests

---
+++ tests/test_player.py
@@ -0,0 +1,13 @@
+import unittest
+
+from .context import penny_ante
+from penny_ante.player import Player
+
+class TestChips(unittest.TestCase):
+
+    def test_create_player(self):
+        test_player = Player(name = 'Bobby')
+        self.assertEqual(test_player.name, 'Bobby')
+
+if __name__ == '__main__':
+    unittest.main()
