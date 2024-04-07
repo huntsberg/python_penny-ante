c22fafa091881d4333eb9dd568350d104b41495c
Peter Bowen <peter@bowenfamily.org>
2024-04-07 16:02:49 -0700

    Updated the exception text cince 0 vs 00 are both the same number but different values

--- src/penny_ante/space.py
+++ src/penny_ante/space.py
@@ -1,7 +1,7 @@
 class Space:
     def __init__(self, value):
         if value == None:
-            raise Exception("To instantiate a space, number is required.")
+            raise Exception("To instantiate a space, a value is required.")
         self.value = str(value)
         self.color = None
         self.wheel_location = None
